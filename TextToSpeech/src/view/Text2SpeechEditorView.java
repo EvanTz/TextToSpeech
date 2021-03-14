5344f8d3216dc487b8d16ba8020f1879b668cb66
DimitrisSintos <dimsintos91@gmail.com>
2021-03-14 16:55:27 +0200

    Stupid Gui

--- TextToSpeech/src/view/Text2SpeechEditorView.java
+++ TextToSpeech/src/view/Text2SpeechEditorView.java
@@ -1,9 +1,36 @@
 package view;
 
-public class Text2SpeechEditorView {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Text2SpeechEditorView {
+	
+	public Text2SpeechEditorView() {
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		
+		JButton button = new JButton("Open file");
+		
+		
+		panel.setBorder(BorderFactory.createEmptyBorder(100,100,70,100));
+		panel.setLayout(new GridLayout(0,1));
+		panel.add(button);
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Text2Speech");
+		frame.pack();
+		frame.setVisible(true);
+		
+		
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Text2SpeechEditorView();
 
 	}
 
