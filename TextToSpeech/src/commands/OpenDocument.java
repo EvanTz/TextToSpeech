bfcddb0cc03596fefcbc4f4062c36fcf9dfb0984
EvanTz <vaggelistzortzis@gmail.com>
2021-03-19 12:34:15 +0200

    enhanced and excel functionality added

--- TextToSpeech/src/commands/OpenDocument.java
+++ TextToSpeech/src/commands/OpenDocument.java
@@ -1,18 +1,27 @@
 package commands;
 
+//"D:\\Users\\Vaggelis\\Documents\\GitHub\\soft eng textToSpeech\\TextToSpeech\\Resources\\InputSamples"
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import javax.swing.filechooser.FileNameExtensionFilter;
-
+import javax.swing.table.DefaultTableModel;
 
 import model.Document;
 
 public class OpenDocument implements ActionListener{
 	private Document doc;
 	private String encoding;
+	private JTextArea textArea = new JTextArea();
+	private JTable table = new JTable();
+	private DefaultTableModel model;
+	private JScrollPane scroll = new JScrollPane();
 	
 	public OpenDocument() {
 		this.doc = new Document();
@@ -32,18 +41,51 @@ public class OpenDocument implements ActionListener{
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			String inputDoc = chooseFile.getSelectedFile().getAbsolutePath();
 			
-			System.out.println("Document selected: " + inputDoc);
+			//System.out.println("Document selected: " + inputDoc);
 			
 			if(!inputDoc.endsWith("docx") && !inputDoc.endsWith("xlsx")) {
 				JOptionPane.showMessageDialog(chooseFile, "Wrong type of document selected.","Warning",JOptionPane.PLAIN_MESSAGE);
 			}
 			
-			if(inputDoc.endsWith("docx")) doc.open(inputDoc, "docx", encoding); 
-			else if(inputDoc.endsWith("xlsx")) doc.open(inputDoc, "xlsx", encoding);
+			if(inputDoc.endsWith("docx")) {
+				doc.open(inputDoc, "docx", encoding); 
+				textArea.setText(getDocument().getContents().toString());
+				scroll.setVisible(true);
+				scroll.setViewportView(textArea);
+			}
+			else if(inputDoc.endsWith("xlsx")) {
+				doc.open(inputDoc, "xlsx", encoding);
+				
+				
+				// TODO this should be implemented in a private method ideally
+				model.setColumnCount(getDocument().getContents().size());
+				model.setRowCount(1);
+				
+				for (String i : getDocument().getContents()){
+					model.setValueAt(i, 0, getDocument().getContents().indexOf(i)); }
+				 
+				
+				scroll.setVisible(true);
+				scroll.setViewportView(table);
+			}
 			
 		}
 		else JOptionPane.showMessageDialog(chooseFile, "No document selected.","Warning",JOptionPane.PLAIN_MESSAGE);
 		
+	}
+	
+	public void setTextArea(JTextArea textArea) {
+		this.textArea = textArea;
+	}
+	public void setTable(JTable table) {
+		this.table = table;
+		
+	}
+	public void setTableModel(DefaultTableModel model) {
+		this.model = model;
+	}
+	public void setScroll(JScrollPane scroll) {
+		this.scroll = scroll;
 	}
 	
 	public void setDocument(Document document) {
@@ -65,5 +107,7 @@ public class OpenDocument implements ActionListener{
 	public void setReplayManager(ReplayManager rm) {
 		
 	}
+
+	
 	
 }
