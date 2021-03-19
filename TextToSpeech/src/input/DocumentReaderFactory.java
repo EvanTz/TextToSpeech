505efd57dcd0713bb16131344e8322b993e9e477
EvanTz <vaggelistzortzis@gmail.com>
2021-03-19 12:35:12 +0200

    reader factory, atbash, rot13 decorators completed

--- TextToSpeech/src/input/DocumentReaderFactory.java
+++ TextToSpeech/src/input/DocumentReaderFactory.java
@@ -3,12 +3,31 @@ package input;
 public class DocumentReaderFactory {
 	private ReaderAtbashDecorator readerAtbashDecorator;
 	private ReaderRot13Decorator readerRot13Decorator;
+	private DocumentReader docReader;
 
 	public DocumentReaderFactory() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	
-	public DocumentReader createReader(String str1, String str2, String str3) {
+	public DocumentReader createReader(String docPath, String docType, String docEncoding) {
+		
+		// TODO should word reader and excel reader take as input the path of the document ????????? :  WordReader(docPath) ExcelReader(docPath)
+		
+		if (docType == "docx") {
+			docReader = new WordReader();
+		}
+		else if(docType == "xlsx") {
+			docReader = new ExcelReader();
+		}
+		
+		if(docEncoding == "Atbash") {
+			readerAtbashDecorator = new ReaderAtbashDecorator(docReader);
+			return readerAtbashDecorator;
+		}
+		else if(docEncoding == "Rot13") {
+			readerRot13Decorator = new ReaderRot13Decorator(docReader);
+			return readerRot13Decorator;
+		}
 		return null;
 	}
 	
--- TextToSpeech/src/input/ReaderAtbashDecorator.java
+++ TextToSpeech/src/input/ReaderAtbashDecorator.java
@@ -1,18 +1,43 @@
 package input;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ReaderAtbashDecorator extends ReaderDecorator{
+	private DocumentReader doc;
+	private List<String> input;
+	private List<String> output;
 
 	public ReaderAtbashDecorator(DocumentReader doc) {
 		super(doc);
-		// TODO Auto-generated constructor stub
+		this.doc = doc;
+		output = new ArrayList<String>();
 	}
 
 	@Override
 	public List<String> read() {
-		// TODO Auto-generated method stub
-		return null;
+		input = doc.read();
+		String tempString = "";
+		
+		// TODO check that input is not null
+		
+		for(String line: input) {
+			tempString = "";
+			for( char c : line.toCharArray()) {
+				if(Character.isLowerCase(c)) {
+					tempString += (char) ('z' + ('a' - c));
+ 				}
+				else if(Character.isUpperCase(c)) {
+					tempString += (char) ('Z' + ('A' - c));
+				}
+				else {
+					tempString += c;
+				}
+			}
+			output.add(tempString);
+		
+		}
+		return output;
 	}
 
 }
